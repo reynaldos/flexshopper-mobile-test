e0261ea925df5a49836cb6e9c95e2caf7bea4c6b
rey sanchez <rey.sanchez.dev@gmail.com>
2024-10-30 11:43:54 -0400

    upgrade footer

---
+++ components/Footer/footerLinks.tsx
@@ -0,0 +1,241 @@
+import { ReactElement } from "react";
+
+export const socialLinks: {
+  title: string;
+  url: string;
+  icon: ReactElement<any, any>;
+}[] = [
+  {
+    title: "FlexShopper on Facebook",
+    url: "https://www.facebook.com/flexshopper",
+    icon: (
+      <svg
+        xmlns="https://www.w3.org/2000/svg"
+        version="1.1"
+        x="0px"
+        y="0px"
+        width="512px"
+        height="512px"
+        viewBox="0 0 49.652 49.652"
+      >
+        <title>FlexShopper on Facebook</title>
+        <g>
+          <g>
+            <path
+              d="M24.826,0C11.137,0,0,11.137,0,24.826c0,13.688,11.137,24.826,24.826,24.826c13.688,0,24.826-11.138,24.826-24.826    C49.652,11.137,38.516,0,24.826,0z M31,25.7h-4.039c0,6.453,0,14.396,0,14.396h-5.985c0,0,0-7.866,0-14.396h-2.845v-5.088h2.845    v-3.291c0-2.357,1.12-6.04,6.04-6.04l4.435,0.017v4.939c0,0-2.695,0-3.219,0c-0.524,0-1.269,0.262-1.269,1.386v2.99h4.56L31,25.7z    "
+              fill="#FFFFFF"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    ),
+  },
+  {
+    title: "FlexShopper on Twitter",
+    url: "https://www.twitter.com/flexshopper",
+    icon: (
+      <svg
+        xmlns="https://www.w3.org/2000/svg"
+        version="1.1"
+        x="0px"
+        y="0px"
+        width="512px"
+        height="512px"
+        viewBox="0 0 49.652 49.652"
+      >
+        <title>FlexShopper on Twitter</title>
+        <g>
+          <g>
+            <path
+              d="M24.826,0C11.137,0,0,11.137,0,24.826c0,13.688,11.137,24.826,24.826,24.826c13.688,0,24.826-11.138,24.826-24.826    C49.652,11.137,38.516,0,24.826,0z M35.901,19.144c0.011,0.246,0.017,0.494,0.017,0.742c0,7.551-5.746,16.255-16.259,16.255    c-3.227,0-6.231-0.943-8.759-2.565c0.447,0.053,0.902,0.08,1.363,0.08c2.678,0,5.141-0.914,7.097-2.446    c-2.5-0.046-4.611-1.698-5.338-3.969c0.348,0.066,0.707,0.103,1.074,0.103c0.521,0,1.027-0.068,1.506-0.199    c-2.614-0.524-4.583-2.833-4.583-5.603c0-0.024,0-0.049,0.001-0.072c0.77,0.427,1.651,0.685,2.587,0.714    c-1.532-1.023-2.541-2.773-2.541-4.755c0-1.048,0.281-2.03,0.773-2.874c2.817,3.458,7.029,5.732,11.777,5.972    c-0.098-0.419-0.147-0.854-0.147-1.303c0-3.155,2.558-5.714,5.713-5.714c1.644,0,3.127,0.694,4.171,1.804    c1.303-0.256,2.523-0.73,3.63-1.387c-0.43,1.335-1.333,2.454-2.516,3.162c1.157-0.138,2.261-0.444,3.282-0.899    C37.987,17.334,37.018,18.341,35.901,19.144z"
+              fill="#FFFFFF"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    ),
+  },
+  {
+    title: "FlexShopper on YouTube",
+    url: "https://www.youtube.com/user/flexshopper",
+    icon: (
+      <svg
+        xmlns="https://www.w3.org/2000/svg"
+        version="1.1"
+        x="0px"
+        y="0px"
+        width="512px"
+        height="512px"
+        viewBox="0 0 97.75 97.75"
+      >
+        <title>FlexShopper on YouTube</title>
+        <g>
+          <g>
+            <polygon
+              points="25.676,52.482 29.551,52.482 29.551,73.455 33.217,73.455 33.217,52.482 37.164,52.482 37.164,49.047     25.676,49.047   "
+              fill="#FFFFFF"
+            ></polygon>
+            <path
+              d="M56.674,55.046c-1.212,0-2.343,0.662-3.406,1.972v-7.972h-3.295v24.409h3.295v-1.762c1.103,1.361,2.233,2.013,3.406,2.013    c1.311,0,2.193-0.69,2.633-2.044c0.221-0.771,0.334-1.982,0.334-3.665v-7.242c0-1.722-0.113-2.924-0.334-3.655    C58.868,55.736,57.984,55.046,56.674,55.046z M56.344,68.255c0,1.644-0.482,2.454-1.434,2.454c-0.541,0-1.092-0.259-1.643-0.811    V58.814c0.551-0.545,1.102-0.803,1.643-0.803c0.951,0,1.434,0.842,1.434,2.482V68.255z"
+              fill="#FFFFFF"
+            ></path>
+            <path
+              d="M43.824,69.167c-0.731,1.033-1.422,1.542-2.084,1.542c-0.44,0-0.691-0.259-0.771-0.771c-0.03-0.106-0.03-0.508-0.03-1.28    v-13.39h-3.296v14.379c0,1.285,0.111,2.153,0.291,2.705c0.331,0.922,1.063,1.354,2.123,1.354c1.213,0,2.457-0.732,3.767-2.234    v1.984h3.298V55.268h-3.298V69.167z"
+              fill="#FFFFFF"
+            ></path>
+            <path
+              d="M46.653,38.466c1.073,0,1.588-0.851,1.588-2.551v-7.731c0-1.701-0.515-2.548-1.588-2.548c-1.074,0-1.59,0.848-1.59,2.548    v7.731C45.063,37.616,45.579,38.466,46.653,38.466z"
+              fill="#FFFFFF"
+            ></path>
+            <path
+              d="M48.875,0C21.882,0,0,21.882,0,48.875S21.882,97.75,48.875,97.75S97.75,75.868,97.75,48.875S75.868,0,48.875,0z     M54.311,22.86h3.321v13.532c0,0.781,0,1.186,0.04,1.295c0.073,0.516,0.335,0.78,0.781,0.78c0.666,0,1.365-0.516,2.104-1.559    V22.86h3.33v18.379h-3.33v-2.004c-1.326,1.52-2.59,2.257-3.805,2.257c-1.072,0-1.812-0.435-2.146-1.365    c-0.184-0.557-0.295-1.436-0.295-2.733V22.86L54.311,22.86z M41.733,28.853c0-1.965,0.334-3.401,1.042-4.33    c0.921-1.257,2.218-1.885,3.878-1.885c1.668,0,2.964,0.628,3.885,1.885c0.698,0.928,1.032,2.365,1.032,4.33v6.436    c0,1.954-0.334,3.403-1.032,4.322c-0.921,1.254-2.217,1.881-3.885,1.881c-1.66,0-2.957-0.627-3.878-1.881    c-0.708-0.919-1.042-2.369-1.042-4.322V28.853z M32.827,16.576l2.622,9.685l2.519-9.685h3.735L37.26,31.251v9.989h-3.692v-9.989    c-0.335-1.77-1.074-4.363-2.259-7.803c-0.778-2.289-1.589-4.585-2.367-6.872H32.827z M75.186,75.061    c-0.668,2.899-3.039,5.039-5.894,5.358c-6.763,0.755-13.604,0.759-20.42,0.755c-6.813,0.004-13.658,0-20.419-0.755    c-2.855-0.319-5.227-2.458-5.893-5.358c-0.951-4.129-0.951-8.638-0.951-12.89s0.012-8.76,0.962-12.89    c0.667-2.9,3.037-5.04,5.892-5.358c6.762-0.755,13.606-0.759,20.421-0.755c6.813-0.004,13.657,0,20.419,0.755    c2.855,0.319,5.227,2.458,5.896,5.358c0.948,4.13,0.942,8.638,0.942,12.89S76.137,70.932,75.186,75.061z"
+              fill="#FFFFFF"
+            ></path>
+            <path
+              d="M67.17,55.046c-1.686,0-2.995,0.619-3.947,1.864c-0.699,0.92-1.018,2.342-1.018,4.285v6.371    c0,1.933,0.357,3.365,1.059,4.276c0.951,1.242,2.264,1.863,3.988,1.863c1.721,0,3.072-0.651,3.984-1.972    c0.4-0.584,0.66-1.245,0.77-1.975c0.031-0.33,0.07-1.061,0.07-2.124v-0.479h-3.361c0,1.32-0.043,2.053-0.072,2.232    c-0.188,0.881-0.662,1.321-1.473,1.321c-1.132,0-1.686-0.84-1.686-2.522v-3.226h6.592v-3.767c0-1.943-0.329-3.365-1.02-4.285    C70.135,55.666,68.824,55.046,67.17,55.046z M68.782,62.218h-3.296v-1.683c0-1.682,0.553-2.523,1.654-2.523    c1.09,0,1.642,0.842,1.642,2.523V62.218z"
+              fill="#FFFFFF"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    ),
+  },
+  {
+    title: "FlexShopper on Instagram",
+    url: "https://www.instagram.com/flexshopper",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        x="0px"
+        y="0px"
+        width="512px"
+        height="512px"
+        viewBox="0 0 49.652 49.652"
+      >
+        <title>FlexShopper on Instagram</title>
+        <g>
+          <g>
+            <g>
+              <path
+                d="M24.825,29.796c2.739,0,4.972-2.229,4.972-4.97c0-1.082-0.354-2.081-0.94-2.897c-0.903-1.252-2.371-2.073-4.029-2.073     c-1.659,0-3.126,0.82-4.031,2.072c-0.588,0.816-0.939,1.815-0.94,2.897C19.854,27.566,22.085,29.796,24.825,29.796z"
+                fill="#FFFFFF"
+              ></path>
+              <polygon
+                points="35.678,18.746 35.678,14.58 35.678,13.96 35.055,13.962 30.891,13.975 30.907,18.762    "
+                fill="#FFFFFF"
+              ></polygon>
+              <path
+                d="M24.826,0C11.137,0,0,11.137,0,24.826c0,13.688,11.137,24.826,24.826,24.826c13.688,0,24.826-11.138,24.826-24.826     C49.652,11.137,38.516,0,24.826,0z M38.945,21.929v11.56c0,3.011-2.448,5.458-5.457,5.458H16.164     c-3.01,0-5.457-2.447-5.457-5.458v-11.56v-5.764c0-3.01,2.447-5.457,5.457-5.457h17.323c3.01,0,5.458,2.447,5.458,5.457V21.929z"
+                fill="#FFFFFF"
+              ></path>
+              <path
+                d="M32.549,24.826c0,4.257-3.464,7.723-7.723,7.723c-4.259,0-7.722-3.466-7.722-7.723c0-1.024,0.204-2.003,0.568-2.897     h-4.215v11.56c0,1.494,1.213,2.704,2.706,2.704h17.323c1.491,0,2.706-1.21,2.706-2.704v-11.56h-4.217     C32.342,22.823,32.549,23.802,32.549,24.826z"
+                fill="#FFFFFF"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </svg>
+    ),
+  },
+];
+
+export const footerLinks: {
+  title: string;
+  url: string;
+}[] = [
+  {
+    title: "Apply Today",
+    url: "https://pay.flexshopper.com/?itm_medium=footer-link&amp;itm_content=apply",
+  },
+  {
+    title: "My Account",
+    url: "https://central.flexshopper.com/?itm_medium=footer-link&amp;itm_content=account",
+  },
+  {
+    title: "How it Works",
+    url: "https://flexshopper.com/page/how-it-works?itm_medium=footer-link&amp;itm_content=how-it-works",
+  },
+  {
+    title: "FAQs",
+    url: "https://flexshopper.com/page/faqs?itm_medium=footer-link&amp;itm_content=faq",
+  },
+  { title: "Reviews", url: "https://flexshopper.com/page/reviews" },
+  {
+    title: "FlexBlog",
+    url: "https://blog.flexshopper.com",
+  },
+  {
+    title: "Investor Relations",
+    url: "https://investors.flexshopper.com?itm_medium=footer-link&amp;itm_content=investors",
+  },
+  { title: "Privacy Policy", url: "https://flexshopper.com/page/privacy" },
+  { title: "Terms of Use", url: "https://flexshopper.com/page/terms-of-use" },
+  {
+    title: "Referral Program Terms",
+    url: "https://flexshopper.com/page/referral-program",
+  },
+  {
+    title: "ESIGN Disclosure",
+    url: "https://flexshopper.com/page/electronic-communication",
+  },
+  {
+    title: "Contact Us",
+    url: "https://flexshopper.com/page/contact-us?itm_medium=footer-link&amp;itm_content=contact-us",
+  },
+  {
+    title: "Become an Affiliate",
+    url: "https://business.flexshopper.com/apply",
+  },
+  {
+    title: "Accessibility",
+    url: "https://flexshopper.com/page/accessibility",
+  },
+  {
+    title: "WhistleBlower Policy",
+    url: "https://flexshopper.com/page/whistleblower-policy",
+  },
+  {
+    title: "Financial Resources",
+    url: "https://www.myfinancialresourcecenter.com",
+  },
+  {
+    title: "FlexAdvantage",
+    url: "https://flexshopper.com/page/flex-advantage",
+  },
+  {
+    title: "FlexWallet",
+    url: "https://www.flex-wallet.com/?itm_medium=footer-link&amp;itm_content=flexwallet",
+  },
+  {
+    title: "FlexTires",
+    url: "https://www.flextires.com/?itm_medium=footer-link&amp;itm_content=flextires",
+  },
+];
+
+export const disclaimers: {
+  superscript: string;
+  content: string;
+}[] = [
+  {
+    superscript: "1",
+    content: `If you are approved for a lease this is your estimated weekly lease payment. On this site, we use the terms "lease," "lease agreement," "rental-purchase agreement" and "agreement" the same. (We usually use the term "Lease.") Under your Lease, you can: (a) weekly, biweekly, or monthly payments or exercise an Early Purchase Option pursuant to your payment offer and some restrictions may apply in CA to acquire Ownership of the rented goods or (b) rent the goods for at least the initial term and then end the lease when you want by making all required payments and returning the goods. In NC, the final payment required to purchase the rented goods will be an amount greater than 10% of the Cash Price. If you are approved for a loan the weekly payment is only an estimate. Please review the actual terms in your loan agreement if you are approved. Your rates, terms and payment schedule are subject to approval pursuant to underwriting criteria and may vary by state. FlexShopper provides offers for Lease to Own Options, however, we do not make any loan or credit decisions and are not representative, brokers or agents for any Lenders.`,
+  },
+  {
+    superscript: "3",
+    content: `If approved by FlexShopper, you can exercise FlexShopper's 90 day same as cash option. After 90 days, you also have an Early Purchase Option that allows you to own item(s) for less. Simply refer to the table in your Lease to look up the Early Purchase Option amount after each payment date. If approved by another payment option provider, please refer to the terms and conditions of your agreement.`,
+  },
+  {
+    superscript: "5",
+    content: `Good credit is not necessary, but not everyone with bad credit is approved. Our goal is to help consumers with imperfect credit get goods they want through a payment offer that works for them.`,
+  },
+  {
+    superscript: "7",
+    content: `An initial payment or security deposit and/or payment may be required before fulfillment.`,
+  },
+  {
+    superscript: "9",
+    content: `All lease/loan decisions and rates and terms are subject to approval pursuant to underwriting criteria related to the applicant's credit quality. Rates and terms may also vary depending upon the state where you reside.`,
+  },
+];
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,57 +1,53 @@
 import React from "react";
+import { disclaimers, footerLinks, socialLinks } from "./footerLinks";
+
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-main700 p-6 py-12 flex flex-col items-center justify-between shadow-md max-w-md mx-auto text-white">
-      <p className="text-sm leading-5 my-4">
+    <footer className="w-full bg-main700 p-6 py-12 flex flex-col items-center justify-between shadow-md  gap-8 max-w-md mx-auto text-white">
+      {/* social links */}
+      <div className="flex gap-4 justify-center footerSocialLinks">
+        {socialLinks.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            title={link.title}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.socialLink}
+          >
+            {link.icon}
+          </a>
+        ))}
+      </div>
+
+      {/* page links */}
+      <div className="flex flex-wrap justify-center gap-x-4 gap-y-2 text-[#aaa] text-center text-sm px-3">
+        {footerLinks.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            className="hover:underline"
+          >
+            {link.title}
+          </a>
+        ))}
+      </div>
+
+      {/* copyright */}
+      <p className="text-sm leading-4">
         ©2024 FlexShopper, LLC. All rights reserved.
       </p>
 
-      <p className="text-xs leading-5">
-        <sup>1</sup> If you are approved for a lease this is your estimated
-        weekly lease payment. On this site, we use the terms &quot;lease,&quot;
-        &quot;lease agreement,&quot; &quot;rental-purchase agreement&quot; and
-        &quot;agreement&quot; the same. (We usually use the term &quot;Lease.&quot;)
-        Under your Lease, you can: (a) weekly, biweekly, or monthly payments or exercise
-        an Early Purchase Option pursuant to your payment offer and some restrictions
-        may apply in CA to acquire Ownership of the rented goods or (b) rent the goods
-        for at least the initial term and then end the lease when you want by making all
-        required payments and returning the goods. In NC, the final payment required to
-        purchase the rented goods will be an amount greater than 10% of the Cash Price.
-        If you are approved for a loan the weekly payment is only an estimate. Please
-        review the actual terms in your loan agreement if you are approved. Your rates,
-        terms and payment schedule are subject to approval pursuant to underwriting
-        criteria and may vary by state. FlexShopper provides offers for Lease to Own
-        Options, however, we do not make any loan or credit decisions and are not
-        representative, brokers or agents for any Lenders.
-      </p>
-
-      <p className="text-xs mt-4 leading-5">
-        <sup>3</sup> If approved by FlexShopper, you can exercise FlexShopper&apos;s
-        90 day same as cash option. After 90 days, you also have an Early Purchase
-        Option that allows you to own item(s) for less. Simply refer to the table in
-        your Lease to look up the Early Purchase Option amount after each payment date.
-        If approved by another payment option provider, please refer to the terms and
-        conditions of your agreement.
-      </p>
-
-      <p className="text-xs mt-4 leading-5">
-        <sup>5</sup> Good credit is not necessary but not everyone with bad credit
-        is approved. Our goal is to help consumers with not so perfect credit get
-        goods they want through a payment offer that works for you.
-      </p>
-
-      <p className="text-xs mt-4 leading-5">
-        <sup>7</sup> An initial payment or security deposit and/or payment may be
-        required before fulfillment.
-      </p>
-
-      <p className="text-xs mt-4 leading-5">
-        <sup>9</sup> All lease/loan decisions and rates and terms are subject to
-        approval pursuant to underwriting criteria related to the applicant&apos;s
-        credit quality. Rates and terms may also vary depending upon the state where
-        you reside.
-      </p>
+      {/* disclaimer text */}
+      <div className="text-[10px] leading-4">
+        {disclaimers.map((disclaimer, index) => (
+          <p key={index} className="mt-4">
+            <sup>{disclaimer.superscript}</sup> {disclaimer.content}
+          </p>
+        ))}
+      </div>
     </footer>
   );
 };
