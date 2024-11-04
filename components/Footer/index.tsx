4e3e0b6f59fdab394a16f8faddc5d7dc036e54a4
rey sanchez <rey.sanchez.dev@gmail.com>
2024-11-04 09:22:16 -0500

    bug fix

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { disclaimers, footerLinks, socialLinks } from "./footerLinks";
 import Image from "next/image";
 
